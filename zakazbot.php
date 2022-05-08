0888f07529168591f20efac45c10702d70edfa56
xojimurod95 <58443572+xojimurod95@users.noreply.github.com>
2022-05-09 01:29:45 +0500

    Update zakazbot.php

--- zakazbot.php
+++ zakazbot.php
@@ -1,13 +1,14 @@
 <?php
-include 'Telegram.php';
+include './Telegram.php';
 
 $telegram = new Telegram('5295272780:AAEqmf_BSf5HjtESko8LxrBy5pwIe5N20JM');
 
 $chat_id = $telegram->ChatID();
 
-$content = array('chat_id'=>$chat_id, 'text'=>'Assalomu aleykum aziz Obunachi, Sizga qanday yordam bera olaman.');
+$content = array('chat_id' => $chat_id,
+                 'text' => 'Assalomu aleykum aziz Obunachi, Sizga qanday yordam bera olaman');
 $telegram->sendMessage($content);
 
 // webHook
-// https://api.telegram.org/bot5295272780:AAEqmf_BSf5HjtESko8LxrBy5pwIe5N20JM/setWebhook?url=https://github.com/xojimurod95/telegramchatbot/edit/main/zakazbot.php
+// https://api.telegram.org/bot5295272780:AAEqmf_BSf5HjtESko8LxrBy5pwIe5N20JM/setWebhook?url=https://github.com/xojimurod95/telegramchatbot/zakazbot.php
 ?>
